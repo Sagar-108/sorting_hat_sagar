7c2f6506b64a23b5ff2df8b1417da43cc6fec8d1
Sagar-108 <sagar.goyal.che20@itbhu.ac.in>
2023-12-06 18:08:27 +0530

    Added comments for better understanding of the code

--- sorting_hat.cpp
+++ sorting_hat.cpp
@@ -1,9 +1,11 @@
+// including required header files
 #include <iostream>
 #include <vector>
 #include <unordered_set>
 
 using namespace std;
 
+// implementing Sttudents class
 class Students
 {
 private:
@@ -19,15 +21,20 @@ public:
         food = myFood;
     }
 
+// method to get student roll number
     int getRollNumber() 
     {
         return roll_num;
     }
 
+// method to get student class
+
     char getStudentClass() 
     {
         return class_name;
     }
+
+// method to get student food preference
 
     string getFoodPreference()
     {
@@ -36,36 +43,50 @@ public:
 };
 
 
+// implementing sorting hat class
 class sortingHat
 {
 
 public:
+
+// dynamically taking input capacity of each boarding house
     sortingHat(int cap){
         capacity = cap;
     }
 
+// method to assign valid boarding house to a student
 void assignHouse(Students student) {
         char clsn = student.getStudentClass();
         int roll = student.getRollNumber();
         string foodp = student.getFoodPreference();
 
+        // check for duplicate entries
         if(registered_set.find(roll) == registered_set.end()){
 
             registered_set.insert(roll);
 
+            // check if the valid house is house_A_V and also ensuring capacity limit
             if(clsn == 'A' && foodp == "V" && house_A_V.size() < capacity){
                 house_A_V.push_back(roll);
             }
+
+            // check if the valid house is house_B_V and also ensuring capacity limit
             else if(clsn == 'B' && foodp == "V" && house_B_V.size() < capacity){
                 house_B_V.push_back(roll);
             }
+
+            // check if the valid house is house_A_NV and also ensuring capacity limit
             else if(clsn == 'A' && foodp == "NV" && house_A_NV.size() < capacity){
                 house_A_NV.push_back(roll);
             }
+
+            // check if the valid house is house_B_NV and also ensuring capacity limit
             else if(clsn == 'B' && foodp == "NV" && house_B_NV.size() < capacity){
                 house_B_NV.push_back(roll);
             }
 
+            // if the capacity of valid house is full then adding the student to NA 
+            // which means the student can not be assigned any boarding house
             else{
                 NA.push_back(roll);
             }
@@ -74,6 +95,8 @@ void assignHouse(Students student) {
     
     }
 
+
+// method to print the rolls of student boarded in each of the four boarding houses
 vector<int> getBoardingHouses() {
         cout<<"A_V: ";
         for(auto i:house_A_V)
@@ -130,14 +153,18 @@ int main(){
         char clas;
         string food;
         cin>>state;
+        // if the state is not reg i.e. the state is fin then take no more input and display the result
         if(state != "reg")
             break;
         cin >> roll >> clas >> food;
 
+        // calling the student class
         Students newStudent(roll, clas, food);
-            sh.assignHouse(newStudent);
+        // assigning boarding house to the newly enrolled student
+        sh.assignHouse(newStudent);
     }
 
+    // displaying the boarding houses with the roll numbers of students enrolled in them
     sh.getBoardingHouses();
 
 
